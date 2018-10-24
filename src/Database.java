1293aa7ec1e192824861520c58ba31ae4b946c17
gpilleux <guillermo.pilleux@ing.uchile.cl>
2018-10-24 08:59:48 -0300

    Estructura de clase Database

---
+++ src/Database.java
@@ -0,0 +1,24 @@
+/**
+ * Created by GPG on 10/24/2018.
+ */
+public class Database {
+    //estructura subyacente: archivo.txt
+    String file;
+
+    /**
+     * @param file: Nombre del archivo (extension txt) donde estan almacenados los nodos
+     */
+    public Database(String file){
+        this.file = file;
+    }
+
+    /**
+     * Metodo que ordena segun un atributo
+     * Utiliza MergeSort
+     * @param attribute atributo por el que ordena
+     */
+    void ordenar(String attribute){
+
+    }
+
+}
